136f6c936601517f2ba1a3ebc87d6a1e7a72fe6a
Sh1n0g1 <shinogi-s@macnica.net>
2017-05-18 01:31:10 +0900

    Update PlayNote.ino
    Fix broken indent.
    Add B button for longer tone.

--- PlayNote.ino
+++ PlayNote.ino
@@ -18,16 +18,12 @@ bool mass_blink=false;
 void setup() {
   arduboy.begin();
   arduboy.setFrameRate(10);
-
   //audio setup
   tunes.initChannel(PIN_SPEAKER_1);
   tunes.initChannel(PIN_SPEAKER_2);
-    
-  
   x=0;
   y=4;
 }
-
 
 void saveNote(float newnote){
   for(int i=7; i > 0; i--){
@@ -40,40 +36,47 @@ void loop() {
   arduboy.clear();
   if (!(arduboy.nextFrame()))
     return;
- if(arduboy.everyXFrames(1)){
   if(arduboy.pressed(UP_BUTTON)){
     y = (y + FORM_HEIGHT -1) % FORM_HEIGHT;    
   }
   if(arduboy.pressed(DOWN_BUTTON)){
-   y = (y + FORM_HEIGHT + 1) % FORM_HEIGHT;
+    y = (y + FORM_HEIGHT + 1) % FORM_HEIGHT;
   }
   if(arduboy.pressed(LEFT_BUTTON)){
-   x = (x + FORM_WIDTH - 1) % FORM_WIDTH;
-   if(x % FORM_WIDTH == FORM_WIDTH - 1 ){
-       y = (y + FORM_HEIGHT - 1) % FORM_HEIGHT; 
-   }
+    x = (x + FORM_WIDTH - 1) % FORM_WIDTH;
+    if(x % FORM_WIDTH == FORM_WIDTH - 1 ){
+      y = (y + FORM_HEIGHT - 1) % FORM_HEIGHT; 
+    }
   }
   if(arduboy.pressed(RIGHT_BUTTON)){
-   x = (x + FORM_WIDTH + 1) % FORM_WIDTH;
-   if(x==0){
-       y = (y + FORM_HEIGHT + 1) % FORM_HEIGHT; 
-   }    
+    x = (x + FORM_WIDTH + 1) % FORM_WIDTH;
+    if(x==0){
+      y = (y + FORM_HEIGHT + 1) % FORM_HEIGHT; 
+    }    
   }
   if(arduboy.pressed(B_BUTTON)){
-     float newnote=C0;
-     for(int i=1; i <= y * FORM_WIDTH + x ; i++){
-        newnote *= TONEUP;
-     }
-     saveNote(newnote);
-     tunes.tone(note[0],NOTE_LENGTH);
+    float newnote=C0;
+    for(int i=1; i <= y * FORM_WIDTH + x ; i++){
+      newnote *= TONEUP;
+    }
+    saveNote(newnote);
+    tunes.tone(note[0],NOTE_LENGTH);
   }
- }
+  if(arduboy.pressed(A_BUTTON)){
+    float newnote=C0;
+    for(int i=1; i <= y * FORM_WIDTH + x ; i++){
+      newnote *= TONEUP;
+    }
+    saveNote(newnote);
+    tunes.tone(note[0],NOTE_LENGTH*2);
+  }
+  //Display
   int MARGIN_X=128 - (MASS_WIDTH * FORM_WIDTH);
   int MARGIN_Y=(64 - MASS_HEIGHT * FORM_HEIGHT)/2;
   for(int i=0; i < FORM_WIDTH; i++){
     for(int j=0; j < FORM_HEIGHT; j++){
       if(i == 1 || i==3 || i==6 || i==8 || i==10){
-         arduboy.drawRect(
+        arduboy.drawRect(
           i * MASS_WIDTH + MARGIN_X,
           j * MASS_HEIGHT + MARGIN_Y,
           MASS_WIDTH +1,
